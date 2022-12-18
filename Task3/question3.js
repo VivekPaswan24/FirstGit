c0c0f97cdf09f6afc3ff6aab4a97764619e06336
VivekPaswan24 <vivek.paswan0@gmail.com>
2022-12-18 16:40:35 +0530

    second file with dom

---
+++ Task3/question3.js
@@ -0,0 +1,13 @@
+//EXAMINE WITH DOCUMENT//
+// console.dir(document)
+// console.log(document.forms)
+// console.log(document.title);
+
+const headerTitle=document.getElementById("header-title");
+const header=document.getElementById("main-header");
+header.style.borderBottom="solid 3px black"
+const additem=document.getElementsByClassName("title")
+console.log(additem)
+additem[0].textContent="ADD ITEMS"
+additem[0].style.fontWeight="bold"
+additem[0].style.color="green"
